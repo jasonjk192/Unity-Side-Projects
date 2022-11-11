40465c0613b168766753d50bc7a264246a21b539
jasonjk192 <73054054+jasonjk192@users.noreply.github.com>
2022-11-11 15:49:36 +0530

    scifi

---
+++ Sci-Fi Demo/Assets/Game/Scripts/Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    [SerializeField]
+    private AudioClip _coinPickUp;
+    UIManager _uiManager;
+    private void Start()
+    {
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                other.GetComponent<Player>().giveCoin(1);
+                AudioSource.PlayClipAtPoint(_coinPickUp,transform.position,1f);
+                _uiManager.UpdateCoin(true);
+                Destroy(this.gameObject,0.1f);
+            }
+        }
+    }
+}
---
+++ Sci-Fi Demo/Assets/Game/Scripts/SharkShop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkShop : MonoBehaviour {
+    [SerializeField]
+    private GameObject _sharkCollision;
+    [SerializeField]
+    private AudioClip _coinPickUp;
+    UIManager _uiManager;
+
+    private void Start()
+    {
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if (other.GetComponent<Player>().getCoin() > 0)
+                {
+                    other.GetComponent<Player>().giveCoin(-1);
+                    _uiManager.UpdateCoin(false);
+                    AudioSource.PlayClipAtPoint(_coinPickUp, transform.position, 1f);
+                    other.GetComponent<Player>().enableWeapon(true);
+                }
+                else
+                    Debug.Log("Get out!");
+            }
+        }
+    }
+}
